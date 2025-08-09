8d5bf55c003d2e17b80e005daa6d15d83303fd82
Brenda <brenda.aylen.1311@gmail.com>
2025-08-09 03:24:19 -0300

    agrega sorteo en js, boton de nuevo sorteo en html, mejora en css

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [] ;
+let amigosSorteados = [];
 
 function agregarAmigo () {
     let nombreDeAmigo = document.getElementById("amigo").value.trim(); //se utiliza .trim para eliminar espacios en blanco al inicio y al final
@@ -26,4 +27,49 @@ function mostrarAmigos() {
 
     }
 }
+
+function sortearAmigo (){
+    if (amigos.length=== 0){
+        alert("No hay amigos para sortear")
+        return;
+    }
+    if (amigosSorteados.length===amigos.length){
+        alert("Todos ya fueron sorteados");
+        return;
+
+    }
+
+let amigoSecreto;
+ do {
+    let indiceAleatorio= Math.floor(Math.random() *amigos.length);
+    amigoSecreto=amigos[indiceAleatorio];
+ } while (amigosSorteados.includes(amigoSecreto));
+
+ amigosSorteados.push(amigoSecreto);
+
+//para mostrar
+document.getElementById("resultado").innerHTML = `Tu amigo secreto es: <strong>${amigoSecreto} </strong>`;
+
+//para añadir tiempo de "muestra" del amigo secreto 5seg
+setTimeout(()=> {
+    document.getElementById("resultado").innerHTML= "";
+    },3000);
+
+}
+
+function nuevoSorteo(){
+    amigos=[]; //vaciar la lista de amigos
+    amigosSorteados = []; // para vaciar la lista de los sorteados
+    document.getElementById("listaAmigos").innerHTML="";
+    document.getElementById("resultado").innerHTML= "";
+    document.getElementById("amigo").value ="";
+
+
+
+}
+
+
+
+
+
 
